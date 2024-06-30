5a04398e2b1c184f4a96fb457922cc62a805316e
Danil <da.demin@innopolis.university>
2024-06-30 22:01:01 +0300

    add XKCD comic

---
+++ js/index.js
@@ -0,0 +1,6 @@
+function comic() {
+  const comic_id = fetch(
+    "https://fwd.innopolis.university/api/hw2?email=da.demin@innopolis.university"
+  );
+  "index.html".getElementById("hui").innerHTML = "lol";
+}
