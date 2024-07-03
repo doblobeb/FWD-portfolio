46a585455c01e8574229ac6ac986f7639f520fae
doblobeb <146896535+doblobeb@users.noreply.github.com>
2024-07-03 13:39:14 +0300

    Delete useless js directory

--- js/index.js
+++
@@ -1,6 +0,0 @@
-function comic() {
-  const comic_id = fetch(
-    "https://fwd.innopolis.university/api/hw2?email=da.demin@innopolis.university"
-  );
-  "index.html".getElementById("hui").innerHTML = "lol";
-}
